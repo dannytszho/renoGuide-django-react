fea3b399d22b589de00e8c8c3b016a575a8df4af
dannytszho <dannytszho@gmail.com>
2022-07-07 13:45:55 -0700

    Render mock data in HikingTrailsPage

--- frontend/src/Components/HikingTrailsPage.tsx
+++ frontend/src/Components/HikingTrailsPage.tsx
@@ -1,5 +1,37 @@
 import TrailsCard from './TrailsCard'
+import { useState } from 'react'
+import { trailsData } from '../TrailsData'
+
+export interface TrailsType {
+  id: string
+  name: string
+  length: string
+  elevation: string
+  duration: string
+  difficulty: 'Easy' | 'Moderate' | 'Hard'
+  rating: string
+  imageUrl: string
+  url: string
+}
+
 const HikingTrailsPage = () => {
-  return <TrailsCard />
+  return (
+    <>
+      <div>
+        {trailsData.map(trail => (
+          <TrailsCard
+            name={trail.name}
+            length={trail.length}
+            elevation={trail.elevation}
+            image={trail.imageUrl}
+            difficulty={trail.difficulty}
+            // rating={<Rating rating={parseFloat(trail.rating)} />}
+            duration={trail.duration}
+            urL={trail.url}
+          />
+        ))}
+      </div>
+    </>
+  )
 }
 export default HikingTrailsPage
