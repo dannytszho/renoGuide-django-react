7f3128821dae7f8dd5736d4eaac233470f1dde2b
dannytszho <dannytszho@gmail.com>
2022-07-07 12:01:12 -0700

    Add Nevada pic

--- frontend/src/Components/HomePage.tsx
+++ frontend/src/Components/HomePage.tsx
@@ -1,7 +1,14 @@
+import Nevada from '../Images/Nevada.png'
 const HomePage = () => {
   return (
     <>
-      <div className="flex flex-col"></div>
+      {/* Hero Section */}
+      <div className="flex flex-col">
+        <div className="flex justify-center mt-20">
+          <img src={Nevada} alt="navada" />
+        </div>
+        <h2 className="text-black">haha</h2>
+      </div>
     </>
   )
 }
