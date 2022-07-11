480f13fbd37fd8c7b48b264976bd067b34cf0941
dannytszho <dannytszho@gmail.com>
2022-07-11 16:34:54 -0700

    Separate frontend and backend

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TrailRetrieveUpdateDestroyView, TrailCreateView
-
-urlpatterns = [
-    path('trails/<int:id>', TrailRetrieveUpdateDestroyView.as_view()),
-    path('trails', TrailCreateView.as_view()),
-]
--- renoGuide/views.py
+++
@@ -1,5 +0,0 @@
-from urllib import request
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'index.html')
